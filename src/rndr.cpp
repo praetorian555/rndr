def41a10a6621b21b00830603ee24a1f57ecc02e
Marko Kostic <archmarko92@gmail.com>
2021-10-30 19:13:49 +0200

    Fix names of log macros

--- src/rndr.cpp
+++ src/rndr.cpp
@@ -6,11 +6,11 @@ void RNDR::HelloWorld()
 {
     Log::Init();
 
-    EXAMPLE_ERROR("Hello there, I am RNDR Library!");
-    EXAMPLE_WARNING("Hello there, I am RNDR Library!");
-    EXAMPLE_INFO("Hello there, I am RNDR Library!");
-    EXAMPLE_DEBUG("Hello there, I am RNDR Library!");
-    EXAMPLE_TRACE("Hello there, I am RNDR Library!");
+    RNDR_ERROR("Hello there, I am RNDR Library!");
+    RNDR_WARNING("Hello there, I am RNDR Library!");
+    RNDR_INFO("Hello there, I am RNDR Library!");
+    RNDR_DEBUG("Hello there, I am RNDR Library!");
+    RNDR_TRACE("Hello there, I am RNDR Library!");
 
     Log::ShutDown();
 }
