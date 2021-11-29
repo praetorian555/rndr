85df52d425def482119c116cac67d440da707a73
Marko Kostic <archmarko92@gmail.com>
2021-11-29 22:09:41 +0100

    Add pixel and vertex shader classes

---
+++ include/rndr/render/shader.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <functional>
+
+#include "rndr/color.h"
+#include "rndr/math/math.h"
+
+namespace rndr
+{
+
+struct PerPixelInfo
+{
+    Point2i Position;
+    real Barycentric[3];
+};
+
+using PixelShaderCallback = std::function<Color(const PerPixelInfo&)>;
+
+struct PixelShader
+{
+    PixelShaderCallback Callback;
+};
+
+struct PerVertexInfo
+{
+    Point3r Position;
+    int PrimitiveIndex;
+    int VertexIndex;
+};
+
+using VertexShaderCallback = std::function<Point3r(const PerVertexInfo&)>;
+
+struct VertexShader
+{
+    VertexShaderCallback Callback;
+};
+
+}  // namespace rndr
