8d89c84bc178e3b827b73a8c558cba923db2d0ae
Marko Kostic <archmarko92@gmail.com>
2021-11-29 22:07:31 +0100

    Remove method for rendering triangle from surface and use only Color class in API

--- include/rndr/surface.h
+++ include/rndr/surface.h
@@ -11,14 +11,6 @@ namespace rndr
 class Window;
 struct Color;
 
-struct PixelShaderInfo
-{
-    Point2r Position;
-    real Barycentric[3];
-};
-
-using PixelShaderCallback = std::function<Color(const PixelShaderInfo&)>;
-
 /**
  * Represents a memory buffer to which user can render.
  *
@@ -64,18 +56,10 @@ public:
     uint32_t GetPixelSize() const { return m_PixelSize; }
 
     /**
-     * Colors a pixel at (X, Y) location.
-     *
-     * @param Location Pixel coordinates.
-     * @param Color The format of color needs to be 0xXXRRGGBB. Color needs to be in linear space.
-     */
-    void SetPixel(const Point2i& Location, uint32_t Color);
-
-    /**
      * Colors a pixel at specified location.
      *
      * @param Location Location of a pixel in screen space.
-     * @param Color Color object in linear space.
+     * @param Color New pixel color.
      */
     void SetPixel(const Point2i& Location, rndr::Color Color);
 
@@ -84,9 +68,9 @@ public:
      *
      * @param X Coordinate along the X axis.
      * @param Y Coordinate along the Y axis.
-     * @param Color The format of color needs to be 0xXXRRGGBB. Color needs to be in linear space.
+     * @param Color New pixel color.
      */
-    void SetPixel(int X, int Y, uint32_t Color);
+    void SetPixel(int X, int Y, rndr::Color Color);
 
     /**
      * Render a line between Start and End pixel locations.
@@ -95,29 +79,19 @@ public:
      * @param End Location of a end pixel.
      * @param Color The format of color needs to be 0xXXRRGGBB.
      */
-    void RenderLine(const Vector2i& Start, const Vector2i& End, uint32_t Color);
+    void RenderLine(const Point2i& Start, const Point2i& End, rndr::Color Color);
 
     /**
      * Color a block that starts at BottomLeft of size Size with Color.
      */
-    void RenderBlock(const Vector2i& BottomLeft, const Vector2i& Size, uint32_t Color);
-
-    /**
-     * Render triangle defined by Points. Note that points should be specified in counter-clockwise
-     * direction.
-     *
-     * @param Points Array of three points in the surface's coordinate space.
-     * @param Callback Function that is invoked for every pixel that is inside the triangle.
-     * Function should return the calculated color of a pixel.
-     */
-    void RenderTriangle(const Point2r (&Points)[3], PixelShaderCallback Callback);
+    void RenderBlock(const Point2i& BottomLeft, const Point2i& Size, rndr::Color Color);
 
     /**
      * Clear the color buffer with specified color.
      *
      * @param Color The new color of the color buffer.
      */
-    void ClearColorBuffer(uint32_t Color);
+    void ClearColorBuffer(rndr::Color Color);
 
 private:
     uint32_t m_Width = 0, m_Height = 0;
--- src/core/surface.cpp
+++ src/core/surface.cpp
@@ -17,20 +17,15 @@ void rndr::Surface::UpdateSize(int Width, int Height)
     if (m_ColorBuffer)
     {
         delete[] m_ColorBuffer;
+        m_ColorBuffer = nullptr;
+    }
+
+    if (m_Width == 0 || m_Height == 0)
+    {
+        return;
     }
 
     m_ColorBuffer = new uint8_t[m_Width * m_Height * m_PixelSize];
-}
-
-void rndr::Surface::SetPixel(const Point2i& Location, uint32_t Color)
-{
-    assert(Location.X >= 0 && Location.X < m_Width);
-    assert(Location.Y >= 0 && Location.Y < m_Height);
-    assert(m_ColorBuffer);
-
-    uint32_t* Pixels = (uint32_t*)m_ColorBuffer;
-    Color = rndr::Color(Color).ToGammaCorrectSpace().ToUInt();
-    Pixels[Location.X + Location.Y * m_Width] = Color;
 }
 
 void rndr::Surface::SetPixel(const Point2i& Location, rndr::Color Color)
@@ -40,20 +35,19 @@ void rndr::Surface::SetPixel(const Point2i& Location, rndr::Color Color)
     assert(m_ColorBuffer);
 
     uint32_t* Pixels = (uint32_t*)m_ColorBuffer;
-    uint32_t ColorInt = Color.ToGammaCorrectSpace().ToUInt();
-    Pixels[Location.X + Location.Y * m_Width] = ColorInt;
+    Pixels[Location.X + Location.Y * m_Width] = Color.ToUInt();
 }
 
-void rndr::Surface::SetPixel(int X, int Y, uint32_t Color)
+void rndr::Surface::SetPixel(int X, int Y, rndr::Color Color)
 {
     SetPixel(Point2i{X, Y}, Color);
 }
 
 // Bresenhams line-drawing algorithm
-void rndr::Surface::RenderLine(const Vector2i& A, const Vector2i& B, uint32_t Color)
+void rndr::Surface::RenderLine(const Point2i& A, const Point2i& B, rndr::Color Color)
 {
-    Vector2i Start = A;
-    Vector2i End = B;
+    Point2i Start = A;
+    Point2i End = B;
 
     if (Start.X > End.X)
     {
@@ -101,7 +95,7 @@ void rndr::Surface::RenderLine(const Vector2i& A, const Vector2i& B, uint32_t Co
     }
 }
 
-void rndr::Surface::ClearColorBuffer(uint32_t Color)
+void rndr::Surface::ClearColorBuffer(rndr::Color Color)
 {
     for (uint32_t Y = 0; Y < m_Height; Y++)
     {
@@ -112,14 +106,14 @@ void rndr::Surface::ClearColorBuffer(uint32_t Color)
     }
 }
 
-void rndr::Surface::RenderBlock(const Vector2i& BottomLeft, const Vector2i& Size, uint32_t Color)
+void rndr::Surface::RenderBlock(const Point2i& BottomLeft, const Point2i& Size, rndr::Color Color)
 {
     assert(BottomLeft.X >= 0 && BottomLeft.X < m_Width);
     assert(BottomLeft.Y >= 0 && BottomLeft.Y < m_Height);
     assert(Size.X > 0);
     assert(Size.Y > 0);
 
-    Vector2i TopRight = BottomLeft + Size;
+    Point2i TopRight = BottomLeft + Size;
     assert(TopRight.X >= 0 && TopRight.X < m_Width);
     assert(TopRight.Y >= 0 && TopRight.Y < m_Height);
 
@@ -128,93 +122,6 @@ void rndr::Surface::RenderBlock(const Vector2i& BottomLeft, const Vector2i& Size
         for (int X = BottomLeft.X; X < TopRight.X; X++)
         {
             SetPixel(X, Y, Color);
-        }
-    }
-}
-
-static bool GetBarycentricCoordinates(const rndr::Point3r& Point,
-                                      const rndr::Point3r (&Points)[3],
-                                      const real TriangleArea,
-                                      real (&BarycentricCoordinates)[3])
-{
-    rndr::Vector3r Edge01 = Points[1] - Points[0];
-    rndr::Vector3r Edge12 = Points[2] - Points[1];
-    rndr::Vector3r Edge20 = Points[0] - Points[2];
-    rndr::Vector3r Edge02 = -Edge20;
-
-    rndr::Vector3r Vec0 = Point - Points[0];
-    rndr::Vector3r Vec1 = Point - Points[1];
-    rndr::Vector3r Vec2 = Point - Points[2];
-
-    rndr::Vector3r N = rndr::Cross(Edge01, Edge02);
-
-    rndr::Vector3r BarVec0 = rndr::Cross(Edge01, Vec0);
-    rndr::Vector3r BarVec1 = rndr::Cross(Edge12, Vec1);
-    rndr::Vector3r BarVec2 = rndr::Cross(Edge20, Vec2);
-
-    BarycentricCoordinates[0] = rndr::Cross(Edge01, Vec0).Length() / TriangleArea / 2;
-    BarycentricCoordinates[1] = rndr::Cross(Edge12, Vec1).Length() / TriangleArea / 2;
-    BarycentricCoordinates[2] = rndr::Cross(Edge20, Vec2).Length() / TriangleArea / 2;
-
-    if (rndr::Dot(N, BarVec0) < 0)
-    {
-        return false;
-    }
-
-    if (rndr::Dot(N, BarVec1) < 0)
-    {
-        return false;
-    }
-
-    if (rndr::Dot(N, BarVec2) < 0)
-    {
-        return false;
-    }
-
-    return true;
-}
-
-static real GetTriangleArea(const rndr::Point3r* Points)
-{
-    rndr::Vector3r Edge01 = Points[1] - Points[0];
-    rndr::Vector3r Edge02 = Points[2] - Points[0];
-
-    return rndr::Cross(Edge01, Edge02).Length() / 2;
-}
-
-void rndr::Surface::RenderTriangle(const Point2r (&Points2D)[3], PixelShaderCallback Callback)
-{
-    Point3r Points[3] = {{Points2D[0].X, Points2D[0].Y, 0},
-                         {Points2D[1].X, Points2D[1].Y, 0},
-                         {Points2D[2].X, Points2D[2].Y, 0}};
-
-    Point3r Min = rndr::Min(rndr::Min(Points[0], Points[1]), Points[2]);
-    Point3r Max = rndr::Max(rndr::Max(Points[0], Points[1]), Points[2]);
-    Min = rndr::Floor(Min);
-    Max = rndr::Ceil(Max);
-
-    Vector3r PixelCenter(0.5, 0.5, 0);
-
-    Min += PixelCenter;
-    Max -= PixelCenter;
-
-    real TriangleArea = GetTriangleArea(Points);
-
-    for (real Y = Min.X; Y <= Max.Y; Y += 1)
-    {
-        for (real X = Min.X; X <= Max.X; X += 1)
-        {
-            Point2r Position2D{X, Y};
-            Point3r Position{X, Y, 0};
-            PixelShaderInfo PixelInfo{{X, Y}};
-            bool IsInsideTriangle =
-                GetBarycentricCoordinates(Position, Points, TriangleArea, PixelInfo.Barycentric);
-            if (IsInsideTriangle)
-            {
-                rndr::Color Color = Callback(PixelInfo);
-                Point2i PixelScreen{(int)(Position2D.X - 0.5), (int)(Position2D.Y - 0.5)};
-                SetPixel(PixelScreen, Color);
-            }
         }
     }
 }
