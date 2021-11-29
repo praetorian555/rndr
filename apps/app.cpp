380ea026a9d2243f902c80002cfb17ce6d2288c1
Marko Kostic <archmarko92@gmail.com>
2021-11-29 22:10:45 +0100

    Add rendering of two triangles in the example app

--- apps/app.cpp
+++ apps/app.cpp
@@ -1,52 +1,64 @@
 #include <iostream>
 
 #include "rndr/color.h"
+#include "rndr/render/pipeline.h"
+#include "rndr/render/softrenderer.h"
 #include "rndr/window.h"
 
 int main()
 {
     rndr::Window Window;
+    rndr::SoftwareRenderer Renderer(&Window.GetSurface());
+
+    rndr::PixelShader Shader;
+    Shader.Callback = [](const rndr::PerPixelInfo& Info)
+    {
+        rndr::Color Result;
+        Result = rndr::Color::Red * Info.Barycentric[0] + rndr::Color::Green * Info.Barycentric[1] +
+                 rndr::Color::Blue * Info.Barycentric[2];
+        return Result;
+    };
+
+    rndr::Pipeline Pipeline;
+    Pipeline.bApplyGammaCorrection = true;
+    Pipeline.PixelShader = &Shader;
+
+    Renderer.SetPipeline(&Pipeline);
 
     while (!Window.IsClosed())
     {
         Window.ProcessEvents();
+
+        if (Window.IsWindowMinimized())
+        {
+            continue;
+        }
 
         rndr::Surface Surface = Window.GetSurface();
 
         uint32_t Width = Surface.GetWidth();
         uint32_t Height = Surface.GetHeight();
 
-        Surface.ClearColorBuffer(0x00000000);
+        Surface.ClearColorBuffer(rndr::Color::Black);
 
-        rndr::Vector2i A(400, 400);
-        rndr::Vector2i B(600, 600);
-        rndr::Vector2i C(500, 650);
-        rndr::Vector2i D(600, 200);
-        rndr::Vector2i E(500, 150);
+        rndr::Point2i A(400, 400);
+        rndr::Point2i B(600, 600);
+        rndr::Point2i C(500, 650);
+        rndr::Point2i D(600, 200);
+        rndr::Point2i E(500, 150);
 
-        Surface.RenderLine(A, B, 0x00FF0000);
-        Surface.RenderLine(A, C, 0x00FF00FF);
-        Surface.RenderLine(A, D, 0x0000FF00);
-        Surface.RenderLine(A, E, 0x00FFFFFF);
+        Surface.RenderLine(A, B, rndr::Color::Red);
+        Surface.RenderLine(A, C, rndr::Color::Green);
+        Surface.RenderLine(A, D, rndr::Color::Blue);
+        Surface.RenderLine(A, E, rndr::Color(0, 1, 1, 1));
 
-        Surface.RenderBlock({100, 100}, {200, 50}, 0x00FFFF00);
+        Surface.RenderBlock({100, 100}, {200, 50}, rndr::Color(0, 1, 1, 1));
 
-        rndr::Point2r Triangle1[3] = {{100, 300}, {300, 300}, {200, 500}};
-        rndr::Point2r Triangle2[3] = {{300, 300}, {300, 500}, {100, 500}};
+        std::vector<rndr::Point3r> Positions = {
+            {100, 300, 0}, {300, 300, 0}, {300, 500, 0}, {100, 500, 0}};
+        std::vector<int> Indices = {0, 1, 2, 0, 2, 3};
 
-        Surface.RenderTriangle(Triangle1,
-                               [](const rndr::PixelShaderInfo& Info)
-                               {
-                                   rndr::Vector3r Red(1, 0, 0);
-                                   rndr::Vector3r Green(0, 1, 0);
-                                   rndr::Vector3r Blue(0, 0, 1);
-                                   rndr::Vector3r Result;
-
-                                   Result = Red * Info.Barycentric[0] +
-                                            Green * Info.Barycentric[1] +
-                                            Blue * Info.Barycentric[2];
-                                   return rndr::Color(Result.X, Result.Y, Result.Z, 1);
-                               });
+        Renderer.DrawTriangles(Positions, Indices);
 
         Window.RenderToWindow();
     }
