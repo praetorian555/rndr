4e7fd62eaf28e4a58f3b5caa20ccbbb206af7352
Marko Kostic <archmarko92@gmail.com>
2021-11-29 22:06:13 +0100

    Update Color class
    Add default constructor, addition and multiplication operators and pass gamma value to gamma conversion methods.
    
    Add static values for default colors.

--- include/rndr/color.h
+++ include/rndr/color.h
@@ -24,6 +24,8 @@ struct Color
 
     Color(real RR, real GG, real BB, real AA) : R(RR), G(GG), B(BB), A(AA) {}
 
+    Color();
+
     /**
      * Convert Color to AARRGGBB form.
      *
@@ -38,7 +40,7 @@ struct Color
      *
      * @note Using gamma of 2 for easier calculation.
      */
-    Color ToGammaCorrectSpace() const;
+    Color ToGammaCorrectSpace(real Gamma) const;
 
     /**
      * Convert gamma corrected color back to linear space.
@@ -47,7 +49,19 @@ struct Color
      *
      * @note Using gamma of 2 for easier calculation.
      */
-    Color ToLinearSpace() const;
+    Color ToLinearSpace(real Gamma) const;
+
+    Color operator*(real Value) const;
+    Color& operator*=(real Value);
+    Color operator+(Color Other) const;
+    Color& operator+=(Color Other);
+
+public:
+    static const rndr::Color Black;
+    static const rndr::Color White;
+    static const rndr::Color Red;
+    static const rndr::Color Green;
+    static const rndr::Color Blue;
 };
 
 }  // namespace rndr
--- src/core/color.cpp
+++ src/core/color.cpp
@@ -2,6 +2,19 @@
 
 #include <cmath>
 
+#include "rndr/math/math.h"
+
+const rndr::Color rndr::Color::Black{0, 0, 0, 1};
+const rndr::Color rndr::Color::White{1, 1, 1, 1};
+const rndr::Color rndr::Color::Red{1, 0, 0, 1};
+const rndr::Color rndr::Color::Green{0, 1, 0, 1};
+const rndr::Color rndr::Color::Blue{0, 0, 1, 1};
+
+rndr::Color::Color()
+{
+    *this = Black;
+}
+
 uint32_t rndr::Color::ToUInt() const
 {
     uint32_t RR = (uint32_t)std::roundf(R * 255);
@@ -18,12 +31,43 @@ uint32_t rndr::Color::ToUInt() const
     return Result;
 }
 
-rndr::Color rndr::Color::ToGammaCorrectSpace() const
+rndr::Color rndr::Color::ToGammaCorrectSpace(real Gamma) const
 {
-    return Color(std::sqrt(R), std::sqrt(G), std::sqrt(B), A);
+    return Color(rndr::ToGammaCorrectSpace(R, Gamma), rndr::ToGammaCorrectSpace(G, Gamma),
+                 rndr::ToGammaCorrectSpace(B, Gamma), A);
 }
 
-rndr::Color rndr::Color::ToLinearSpace() const
+rndr::Color rndr::Color::ToLinearSpace(real Gamma) const
 {
-    return Color(R * R, G * G, B * B, A);
+    return Color(rndr::ToLinearSpace(R, Gamma), rndr::ToLinearSpace(G, Gamma),
+                 rndr::ToLinearSpace(B, Gamma), A);
+}
+
+rndr::Color rndr::Color::operator*(real Value) const
+{
+    return Color{R * Value, G * Value, B * Value, A};
+}
+
+rndr::Color& rndr::Color::operator*=(real Value)
+{
+    R *= Value;
+    G *= Value;
+    B *= Value;
+
+    return *this;
+}
+
+rndr::Color rndr::Color::operator+(Color Other) const
+{
+    return Color(R + Other.R, G + Other.G, B + Other.B, std::min(A + Other.A, (real)1));
+}
+
+rndr::Color& rndr::Color::operator+=(Color Other)
+{
+    R += Other.R;
+    G += Other.G;
+    B += Other.B;
+    A = std::min(A + Other.A, (real)1);
+
+    return *this;
 }
