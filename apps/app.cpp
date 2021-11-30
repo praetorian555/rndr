0632aa9f00d6c262dcd35d5a94f31ebabd0ab419
Marko Kostic <archmarko92@gmail.com>
2021-11-30 21:04:09 +0100

    Remove everything but triangles from example app

--- apps/app.cpp
+++ apps/app.cpp
@@ -41,22 +41,9 @@ int main()
 
         Surface.ClearColorBuffer(rndr::Color::Black);
 
-        rndr::Point2i A(400, 400);
-        rndr::Point2i B(600, 600);
-        rndr::Point2i C(500, 650);
-        rndr::Point2i D(600, 200);
-        rndr::Point2i E(500, 150);
-
-        Surface.RenderLine(A, B, rndr::Color::Red);
-        Surface.RenderLine(A, C, rndr::Color::Green);
-        Surface.RenderLine(A, D, rndr::Color::Blue);
-        Surface.RenderLine(A, E, rndr::Color(0, 1, 1, 1));
-
-        Surface.RenderBlock({100, 100}, {200, 50}, rndr::Color(0, 1, 1, 1));
-
         std::vector<rndr::Point3r> Positions = {
-            {100, 300, 0}, {300, 300, 0}, {300, 500, 0}, {100, 500, 0}};
-        std::vector<int> Indices = {0, 1, 2, 0, 2, 3};
+            {300, 300, 0}, {500, 300, 0}, {300, 500, 0}, {500, 500, 0}};
+        std::vector<int> Indices = {0, 1, 2, 1, 2, 3};
 
         Renderer.DrawTriangles(Positions, Indices);
 
