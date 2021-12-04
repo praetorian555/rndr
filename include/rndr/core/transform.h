8c54a5704610a47e39262fcece887939c172c314
Marko Kostic <archmarko92@gmail.com>
2021-12-04 14:56:42 +0100

    Add Transform class

---
+++ include/rndr/core/transform.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include "rndr/core/bounds3.h"
+#include "rndr/core/math.h"
+
+namespace rndr
+{
+
+class Transform
+{
+public:
+    Transform() = default;
+    Transform(const real mat[4][4]);
+    Transform(const Matrix4x4& mat);
+    Transform(const Matrix4x4& mat, const Matrix4x4& invMat);
+
+    const Matrix4x4& GetMatrix() const { return m_Matrix; }
+    const Matrix4x4& GetInverse() const { return m_MatrixInverse; }
+
+    friend Transform Inverse(const Transform& t);
+    friend Transform Transpose(const Transform& t);
+
+    bool operator==(const Transform& other) const;
+    bool operator!=(const Transform& other) const;
+
+    bool IsIdentity() const;
+
+    bool HasScale() const;
+
+    Transform operator*(const Transform& other) const;
+
+    bool SwapsHandedness() const;
+
+    template <typename T>
+    Vector3<T> operator()(const Vector3<T>& v) const;
+    template <typename T>
+    Point3<T> operator()(const Point3<T>& p) const;
+    template <typename T>
+    inline Normal3<T> operator()(const Normal3<T>& n) const;
+    Bounds3f operator()(const Bounds3f& b) const;
+
+    template <typename T>
+    inline Point3<T> operator()(const Point3<T>& pt, Vector3<T>* absError) const;
+    template <typename T>
+    inline Point3<T> operator()(const Point3<T>& p,
+                                const Vector3<T>& pError,
+                                Vector3<T>* absError) const;
+    template <typename T>
+    inline Vector3<T> operator()(const Vector3<T>& v, Vector3<T>* absError) const;
+    template <typename T>
+    inline Vector3<T> operator()(const Vector3<T>& v,
+                                 const Vector3<T>& vError,
+                                 Vector3<T>* absError) const;
+
+private:
+    Matrix4x4 m_Matrix;
+    Matrix4x4 m_MatrixInverse;
+};
+
+Transform Translate(const Vector3r& delta);
+Transform Scale(real x, real y, real z);
+Transform RotateX(real theta);
+Transform RotateY(real theta);
+Transform RotateZ(real theta);
+Transform Rotate(real theta, const Vector3r& axis);
+Transform LookAt(const Point3r& pos, const Point3r& look, const Vector3r& up);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+Vector3<T> Transform::operator()(const Vector3<T>& v) const
+{
+    T x = v.X, y = v.Y, z = v.Z;
+    return Vector3<T>(m_Matrix.m[0][0] * x + m_Matrix.m[0][1] * y + m_Matrix.m[0][2] * z,
+                      m_Matrix.m[1][0] * x + m_Matrix.m[1][1] * y + m_Matrix.m[1][2] * z,
+                      m_Matrix.m[2][0] * x + m_Matrix.m[2][1] * y + m_Matrix.m[2][2] * z);
+}
+
+template <typename T>
+Point3<T> Transform::operator()(const Point3<T>& p) const
+{
+    T x = p.X, y = p.Y, z = p.Z;
+
+    T xp = m_Matrix.m[0][0] * x + m_Matrix.m[0][1] * y + m_Matrix.m[0][2] * z + m_Matrix.m[0][3];
+    T yp = m_Matrix.m[1][0] * x + m_Matrix.m[1][1] * y + m_Matrix.m[1][2] * z + m_Matrix.m[1][3];
+    T zp = m_Matrix.m[2][0] * x + m_Matrix.m[2][1] * y + m_Matrix.m[2][2] * z + m_Matrix.m[2][3];
+    T wp = m_Matrix.m[3][0] * x + m_Matrix.m[3][1] * y + m_Matrix.m[3][2] * z + m_Matrix.m[3][3];
+
+    assert(wp != 0);
+
+    if (wp == 1)
+    {
+        return Point3<T>(xp, yp, zp);
+    }
+    else
+    {
+        return Point3<T>(xp, yp, zp) / wp;
+    }
+}
+
+template <typename T>
+inline Normal3<T> Transform::operator()(const Normal3<T>& n) const
+{
+    T x = n.x, y = n.y, z = n.z;
+    return Normal3<T>(
+        m_MatrixInverse.m[0][0] * x + m_MatrixInverse.m[1][0] * y + m_MatrixInverse.m[2][0] * z,
+        m_MatrixInverse.m[0][1] * x + m_MatrixInverse.m[1][1] * y + m_MatrixInverse.m[2][1] * z,
+        m_MatrixInverse.m[0][2] * x + m_MatrixInverse.m[1][2] * y + m_MatrixInverse.m[2][2] * z);
+}
+
+}  // namespace rndr
---
+++ src/core/transform.cpp
@@ -0,0 +1,255 @@
+#include "rndr/core/transform.h"
+
+namespace rndr
+{
+
+Transform::Transform(const real mat[4][4]) : m_Matrix(mat), m_MatrixInverse(m_Matrix.Inverse()) {}
+
+Transform::Transform(const Matrix4x4& mat) : m_Matrix(mat), m_MatrixInverse(m_Matrix.Inverse()) {}
+
+Transform::Transform(const Matrix4x4& mat, const Matrix4x4& invMat)
+    : m_Matrix(mat), m_MatrixInverse(invMat)
+{
+}
+
+Transform Inverse(const Transform& t)
+{
+    return Transform(t.m_MatrixInverse, t.m_Matrix);
+}
+
+Transform Transpose(const Transform& t)
+{
+    return Transform(t.m_Matrix.Transpose(), t.m_MatrixInverse.Transpose());
+}
+
+bool Transform::operator==(const Transform& other) const
+{
+    return m_Matrix == other.m_Matrix && other.m_MatrixInverse == other.m_MatrixInverse;
+}
+
+bool Transform::operator!=(const Transform& other) const
+{
+    return !(*this == other);
+}
+
+bool Transform::IsIdentity() const
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i == j)
+            {
+                if (m_Matrix.m[i][j] != 1.0f)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (m_Matrix.m[i][j] != 0.0f)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+bool Transform::HasScale() const
+{
+    real la2 = (*this)(Vector3r(1, 0, 0)).LengthSquared();
+    real lb2 = (*this)(Vector3r(0, 1, 0)).LengthSquared();
+    real lc2 = (*this)(Vector3r(0, 0, 1)).LengthSquared();
+#define NOT_ONE(x) ((x) < .999f || (x) > 1.001f)
+    return (NOT_ONE(la2) || NOT_ONE(lb2) || NOT_ONE(lc2));
+#undef NOT_ONE
+}
+
+Transform Transform::operator*(const Transform& other) const
+{
+    Matrix4x4 m = Multiply(m_Matrix, other.m_Matrix);
+    Matrix4x4 minv = Multiply(other.m_MatrixInverse, m_MatrixInverse);
+
+    return Transform(m, minv);
+}
+
+bool Transform::SwapsHandedness() const
+{
+    real det = m_Matrix.m[0][0] *
+                   (m_Matrix.m[1][1] * m_Matrix.m[2][2] - m_Matrix.m[1][2] * m_Matrix.m[2][1]) -
+               m_Matrix.m[0][1] *
+                   (m_Matrix.m[1][0] * m_Matrix.m[2][2] - m_Matrix.m[1][2] * m_Matrix.m[2][0]) +
+               m_Matrix.m[0][2] *
+                   (m_Matrix.m[1][0] * m_Matrix.m[2][1] - m_Matrix.m[1][1] * m_Matrix.m[2][0]);
+    return det < 0;
+}
+
+Transform Translate(const Vector3r& delta)
+{
+    // clang-format off
+    Matrix4x4 m(
+        1, 0, 0, delta.X,
+        0, 1, 0, delta.Y,
+        0, 0, 1, delta.Z,
+        0, 0, 0,       1
+    );
+
+    Matrix4x4 minv(
+        1, 0, 0, -delta.X,
+        0, 1, 0, -delta.Y,
+        0, 0, 1, -delta.Z,
+        0, 0, 0,        1
+    );
+    // clang-format on
+
+    return Transform(m, minv);
+}
+
+Transform Scale(real x, real y, real z)
+{
+    // clang-format off
+    Matrix4x4 m(
+        x, 0, 0, 0,
+        0, y, 0, 0,
+        0, 0, z, 0,
+        0, 0, 0, 1
+    );
+
+    Matrix4x4 minv(
+        1 / x,     0,     0, 0,
+            0, 1 / y,     0, 0,
+            0,     0, 1 / z, 0,
+            0,     0,     0, 1
+    );
+    // clang-format on
+
+    return Transform(m, minv);
+}
+
+Transform RotateX(real theta)
+{
+    real sinTheta = std::sin(Radians(theta));
+    real cosTheta = std::cos(Radians(theta));
+
+    // clang-format off
+    Matrix4x4 m(
+        1,        0,         0, 0, 
+        0, cosTheta, -sinTheta, 0,
+        0, sinTheta,  cosTheta, 0,
+        0,        0,         0, 1);
+    // clang-format on
+
+    return Transform(m, m.Transpose());
+}
+
+Transform RotateY(real theta)
+{
+    real sinTheta = std::sin(Radians(theta));
+    real cosTheta = std::cos(Radians(theta));
+
+    // clang-format off
+    Matrix4x4 m(
+         cosTheta, 0, sinTheta, 0, 
+                0, 1,        0, 0,
+        -sinTheta, 0, cosTheta, 0,
+                0, 0,        0, 1);
+    // clang-format on
+
+    return Transform(m, m.Transpose());
+}
+
+Transform RotateZ(real theta)
+{
+    real sinTheta = std::sin(Radians(theta));
+    real cosTheta = std::cos(Radians(theta));
+
+    // clang-format off
+    Matrix4x4 m(
+        cosTheta, -sinTheta, 0, 0, 
+        sinTheta,  cosTheta, 0, 0,
+               0,         0, 1, 0,
+               0,         0, 0, 1);
+    // clang-format on
+
+    return Transform(m, m.Transpose());
+}
+
+Transform Rotate(real theta, const Vector3r& axis)
+{
+    Vector3r a = Normalize(axis);
+    real sinTheta = std::sin(Radians(theta));
+    real cosTheta = std::cos(Radians(theta));
+    Matrix4x4 m;
+
+    // Compute rotation of first basis vector
+    m.m[0][0] = a.X * a.X + (1 - a.X * a.X) * cosTheta;
+    m.m[0][1] = a.X * a.Y * (1 - cosTheta) - a.Z * sinTheta;
+    m.m[0][2] = a.X * a.Z * (1 - cosTheta) + a.Y * sinTheta;
+    m.m[0][3] = 0;
+
+    // Compute rotations of second and third basis vectors
+    m.m[1][0] = a.X * a.Y * (1 - cosTheta) + a.Z * sinTheta;
+    m.m[1][1] = a.Y * a.Y + (1 - a.Y * a.Y) * cosTheta;
+    m.m[1][2] = a.Y * a.Z * (1 - cosTheta) - a.X * sinTheta;
+    m.m[1][3] = 0;
+
+    m.m[2][0] = a.X * a.Z * (1 - cosTheta) - a.Y * sinTheta;
+    m.m[2][1] = a.Y * a.Z * (1 - cosTheta) + a.X * sinTheta;
+    m.m[2][2] = a.Z * a.Z + (1 - a.Z * a.Z) * cosTheta;
+    m.m[2][3] = 0;
+
+    return Transform(m, m.Transpose());
+}
+
+Transform LookAt(const Point3r& pos, const Point3r& look, const Vector3r& up)
+{
+    Matrix4x4 cameraToWorld;
+
+    cameraToWorld.m[0][3] = pos.X;
+    cameraToWorld.m[1][3] = pos.Y;
+    cameraToWorld.m[2][3] = pos.Z;
+    cameraToWorld.m[3][3] = 1;
+
+    // This is z-axis in left-handend system, if we used right-handed we would have to negate it
+    Vector3r zAxis = Normalize(look - pos);
+    Vector3r xAxis = Cross(Normalize(up), zAxis);
+    Vector3r yAxis = Cross(zAxis, xAxis);
+
+    cameraToWorld.m[0][0] = xAxis.X;
+    cameraToWorld.m[1][0] = xAxis.Y;
+    cameraToWorld.m[2][0] = xAxis.Z;
+    cameraToWorld.m[3][0] = 0.0f;
+
+    cameraToWorld.m[0][1] = yAxis.X;
+    cameraToWorld.m[1][1] = yAxis.Y;
+    cameraToWorld.m[2][1] = yAxis.Z;
+    cameraToWorld.m[3][1] = 0.0f;
+
+    cameraToWorld.m[0][2] = zAxis.X;
+    cameraToWorld.m[1][2] = zAxis.Y;
+    cameraToWorld.m[2][2] = zAxis.Z;
+    cameraToWorld.m[3][2] = 0.0f;
+
+    return Transform(cameraToWorld.Inverse(), cameraToWorld);
+}
+
+Bounds3f Transform::operator()(const Bounds3f& b) const
+{
+    // TODO(mkostic): Make this more efficient
+
+    const Transform& M = *this;
+    Bounds3f ret(M(Point3r(b.pMin.X, b.pMin.Y, b.pMin.Z)));
+    ret = Union(ret, M(Point3r(b.pMax.X, b.pMin.Y, b.pMin.Z)));
+    ret = Union(ret, M(Point3r(b.pMin.X, b.pMax.Y, b.pMin.Z)));
+    ret = Union(ret, M(Point3r(b.pMin.X, b.pMin.Y, b.pMax.Z)));
+    ret = Union(ret, M(Point3r(b.pMin.X, b.pMax.Y, b.pMax.Z)));
+    ret = Union(ret, M(Point3r(b.pMax.X, b.pMax.Y, b.pMin.Z)));
+    ret = Union(ret, M(Point3r(b.pMax.X, b.pMin.Y, b.pMax.Z)));
+    ret = Union(ret, M(Point3r(b.pMax.X, b.pMax.Y, b.pMax.Z)));
+    return ret;
+}
+
+}  // namespace rndr
