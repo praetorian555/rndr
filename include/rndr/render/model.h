4edbd18d683a143e42574c300dacaa4fc717bf84
Marko Kostic <archmarko92@gmail.com>
2022-03-30 10:24:41 +0200

    Modify Model API to have info about the vertex and instance counts

--- include/rndr/render/model.h
+++ include/rndr/render/model.h
@@ -27,6 +27,7 @@ public:
         m_VertexDataStride = sizeof(T);
         m_VertexData.resize(VertexData.size() * m_VertexDataStride);
         memcpy(m_VertexData.data(), VertexData.data(), m_VertexData.capacity());
+        m_VertexCount = VertexData.size();
     }
 
     void SetIndices(const std::vector<int>& Indices) { m_Indices = Indices; }
@@ -37,6 +38,7 @@ public:
         m_InstanceDataStride = sizeof(T);
         m_InstanceData.resize(InstanceData.size() * m_InstanceDataStride);
         memcpy(m_InstanceData.data(), InstanceData.data(), m_InstanceData.capacity());
+        m_InstanceDataCount = InstanceData.size();
     }
 
     template <typename T>
@@ -52,6 +54,7 @@ public:
     std::vector<uint8_t>& GetVertexData() { return m_VertexData; }
     const std::vector<uint8_t>& GetVertexData() const { return m_VertexData; }
     int GetVertexDataStride() const { return m_VertexDataStride; }
+    int GetVertexCount() const { return m_VertexCount; }
 
     std::vector<int>& GetIndices() { return m_Indices; }
     const std::vector<int>& GetIndices() const { return m_Indices; }
@@ -59,6 +62,7 @@ public:
     std::vector<uint8_t>& GetInstanceData() { return m_InstanceData; }
     const std::vector<uint8_t>& GetInstanceData() const { return m_InstanceData; }
     int GetInstanceDataStride() const { return m_InstanceDataStride; }
+    int GetInstanceCount() const { return m_InstanceCount; }
 
     void* GetConstants() { return m_Constants; }
     const void* GetConstants() const { return m_Constants; }
@@ -68,11 +72,13 @@ private:
 
     std::vector<uint8_t> m_VertexData;
     int m_VertexDataStride = 0;
+    int m_VertexCount = 0;
 
     std::vector<int> m_Indices;
 
     std::vector<uint8_t> m_InstanceData;
     int m_InstanceDataStride = 0;
+    int m_InstanceCount = 0;
 
     void* m_Constants = nullptr;
 };
