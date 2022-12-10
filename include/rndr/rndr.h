ac3e968c0af2d03cb8a84b2954736a299668e15c
Marko Kostic <archmarko92@gmail.com>
2022-12-10 17:16:53 +0000

    Remove unused API

--- include/rndr/rndr.h
+++ include/rndr/rndr.h
@@ -1,27 +1,27 @@
 #pragma once
 
+// Core utilities
 #include "rndr/core/fileutils.h"
 #include "rndr/core/input.h"
 #include "rndr/core/log.h"
-#include "rndr/core/objparser.h"
 #include "rndr/core/projectioncamera.h"
 #include "rndr/core/rndrcontext.h"
 #include "rndr/core/span.h"
 #include "rndr/core/threading.h"
 #include "rndr/core/window.h"
 
+// Rendering API
 #include "rndr/render/buffer.h"
 #include "rndr/render/colors.h"
 #include "rndr/render/framebuffer.h"
 #include "rndr/render/graphicscontext.h"
 #include "rndr/render/graphicstypes.h"
 #include "rndr/render/image.h"
-#include "rndr/render/material.h"
-#include "rndr/render/mesh.h"
-#include "rndr/render/model.h"
 #include "rndr/render/pipeline.h"
 #include "rndr/render/shader.h"
 
+// Profiling API
 #include "rndr/profiling/cputracer.h"
 
+// Camera API
 #include "rndr/camera/firstpersoncamera.h"
