340f88f991e751cb00c91d3f671bd7202f2e27c6
Marko Kostic <archmarko92@gmail.com>
2022-12-25 20:33:20 +0000

    Add support for rendering text in basic2d

--- examples/basic2D/src/basic2D.cpp
+++ examples/basic2D/src/basic2D.cpp
@@ -43,44 +43,47 @@ public:
         return std::max(R.Size.X, R.Size.Y) / std::min(R.Size.X, R.Size.Y) * R.Size.X * R.Size.Y;
     }
 
-    bool Compare(const RectIn& A, const RectIn& B) const
+    // Returns true if A is greater than B
+    bool GreaterThan(const RectIn& A, const RectIn& B) const
     {
         switch (m_SortCriteria)
         {
             case SortCriteria::Height:
             {
-                return A.Size.Y < B.Size.Y;
+                return A.Size.Y > B.Size.Y;
             }
             case SortCriteria::Width:
             {
-                return A.Size.X < B.Size.X;
+                return A.Size.X > B.Size.X;
             }
             case SortCriteria::Area:
             {
-                return A.Size.X * A.Size.Y < B.Size.X * B.Size.Y;
+                return A.Size.X * A.Size.Y > B.Size.X * B.Size.Y;
             }
             case SortCriteria::PathologicalMultiplier:
             {
-                return PathologicMult(A) < PathologicMult(B);
+                return PathologicMult(A) > PathologicMult(B);
             }
         }
         assert(false);
         return true;
     }
 
-    bool Compare(const RectOut& A, const RectOut& B)
+    bool GreaterThan(const RectOut& A, const RectOut& B)
     {
         RectIn AA{A.Size};
         RectIn BB{B.Size};
-        return Compare(AA, BB);
+        return GreaterThan(AA, BB);
     }
 
     std::vector<RectOut> Pack(std::vector<RectIn>& InRects)
     {
         // Sort input rectangles based on the sorting criteria
-        std::sort(InRects.begin(), InRects.end(), [this](const RectIn& A, const RectIn& B) { return Compare(A, B); });
+        std::sort(InRects.begin(), InRects.end(),
+                  [this](const RectIn& A, const RectIn& B) { return GreaterThan(A, B); });
 
-        // Initialize array of free slots and populate it by one slot which has the size of the whole atlas
+        // Initialize array of free slots and populate it by one slot which has the size of the
+        // whole atlas
         std::vector<RectOut> Slots;
         RectOut StartSlot{IntPoint{0, 0}, m_AtlasSize, 0};
         Slots.push_back(StartSlot);
@@ -116,14 +119,15 @@ public:
                 const int DiffY = Slot.Size.Y - InRect.Size.Y;
 
                 RectOut SmallerSlot;
-                SmallerSlot.BottomLeft = IntPoint{Slot.BottomLeft.X + Slot.Size.X, Slot.BottomLeft.Y};
+                SmallerSlot.BottomLeft =
+                    IntPoint{Slot.BottomLeft.X + InRect.Size.X, Slot.BottomLeft.Y};
                 SmallerSlot.Size = IntPoint{Slot.Size.X - InRect.Size.X, InRect.Size.Y};
 
                 RectOut BiggerSlot;
                 BiggerSlot.BottomLeft = IntPoint{Slot.BottomLeft.X, InRect.Size.Y};
                 BiggerSlot.Size = IntPoint{Slot.Size.X, Slot.Size.Y - InRect.Size.Y};
 
-                if (Compare(BiggerSlot, SmallerSlot))
+                if (GreaterThan(SmallerSlot, BiggerSlot))
                 {
                     std::swap(SmallerSlot, BiggerSlot);
                 }
@@ -132,11 +136,11 @@ public:
                 std::swap(Slot, Slots.back());
                 Slots.pop_back();
 
-                if (BiggerSlot.Size.X == 0 || BiggerSlot.Size.Y == 0)
+                if (BiggerSlot.Size.X != 0 && BiggerSlot.Size.Y != 0)
                 {
                     Slots.push_back(BiggerSlot);
                 }
-                if (SmallerSlot.Size.X == 0 || SmallerSlot.Size.Y == 0)
+                if (SmallerSlot.Size.X != 0 && SmallerSlot.Size.Y != 0)
                 {
                     Slots.push_back(SmallerSlot);
                 }
@@ -146,6 +150,35 @@ public:
         }
 
         return OutRects;
+    }
+
+    static void Test()
+    {
+        AtlasPacker P(100, 100, SortCriteria::Height);
+        std::vector<RectIn> InRects;
+        InRects.push_back(RectIn{{10, 20}, 0});
+        InRects.push_back(RectIn{{5, 5}, 1});
+        InRects.push_back(RectIn{{20, 10}, 2});
+        InRects.push_back(RectIn{{30, 30}, 3});
+
+        std::vector<RectOut> OutRects = P.Pack(InRects);
+        assert(OutRects.size() == 4);
+
+        assert(OutRects[0].BottomLeft.X == 0);
+        assert(OutRects[0].BottomLeft.Y == 0);
+        assert(OutRects[0].UserData == 3);
+
+        assert(OutRects[1].BottomLeft.X == 30);
+        assert(OutRects[1].BottomLeft.Y == 0);
+        assert(OutRects[1].UserData == 0);
+
+        assert(OutRects[2].BottomLeft.X == 30);
+        assert(OutRects[2].BottomLeft.Y == 20);
+        assert(OutRects[2].UserData == 2);
+
+        assert(OutRects[3].BottomLeft.X == 50);
+        assert(OutRects[3].BottomLeft.Y == 20);
+        assert(OutRects[3].UserData == 1);
     }
 
 private:
@@ -189,6 +222,7 @@ private:
 
     struct Glyph
     {
+        int CodePoint = 0;
         int SpriteId = InvalidSpriteId;
 
         float ScaledYOffset = 0.0f;
@@ -211,7 +245,10 @@ private:
     };
 
 public:
-    Renderer(rndr::GraphicsContext* Ctx, int32_t MaxInstances, int32_t MaxAtlasCount, const math::Vector2& ScreenSize)
+    Renderer(rndr::GraphicsContext* Ctx,
+             int32_t MaxInstances,
+             int32_t MaxAtlasCount,
+             const math::Vector2& ScreenSize)
         : m_Ctx(Ctx), m_MaxInstances(MaxInstances), m_ScreenSize(ScreenSize)
     {
         const std::string VertexShaderPath = BASIC2D_ASSET_DIR "/basic2Dvertex.hlsl";
@@ -219,7 +256,8 @@ public:
 
         const rndr::ByteSpan VertexShaderContents = rndr::file::ReadEntireFile(VertexShaderPath);
         assert(VertexShaderContents);
-        const rndr::ByteSpan FragmentShaderContents = rndr::file::ReadEntireFile(FragmentShaderPath);
+        const rndr::ByteSpan FragmentShaderContents =
+            rndr::file::ReadEntireFile(FragmentShaderPath);
         assert(FragmentShaderContents);
 
         rndr::PipelineProperties PipelineProps{
@@ -265,7 +303,8 @@ public:
         rndr::ImageProperties AtlasProps;
         AtlasProps.PixelFormat = rndr::PixelFormat::R8G8B8A8_UNORM;
         AtlasProps.ImageBindFlags = rndr::ImageBindFlags::ShaderResource;
-        std::vector<uint8_t> WhiteTextureData(AtlasWidth * AtlasHeight * rndr::GetPixelSize(AtlasProps.PixelFormat));
+        std::vector<uint8_t> WhiteTextureData(AtlasWidth * AtlasHeight *
+                                              rndr::GetPixelSize(AtlasProps.PixelFormat));
         memset(WhiteTextureData.data(), 0xFF, WhiteTextureData.size());
         std::vector<rndr::ByteSpan> InitData(MaxAtlasCount);
         for (rndr::ByteSpan& S : InitData)
@@ -274,7 +313,8 @@ public:
             S.Size = WhiteTextureData.size();
         }
         rndr::Span<rndr::ByteSpan> InitDataSpan(InitData);
-        m_TextureAtlas = m_Ctx->CreateImageArray(AtlasWidth, AtlasHeight, MaxAtlasCount, AtlasProps, InitDataSpan);
+        m_TextureAtlas = m_Ctx->CreateImageArray(AtlasWidth, AtlasHeight, MaxAtlasCount, AtlasProps,
+                                                 InitDataSpan);
         assert(m_TextureAtlas.IsValid());
 
         m_TextureAtlasSampler = m_Ctx->CreateSampler();
@@ -295,9 +335,18 @@ public:
         }
 
         Sprite S;
-        S.Id = m_Sprites.size();
+        S.Id = m_Sprites.size() + 1;
         S.ImageData = Im;
         m_Sprites.push_back(S);
+    }
+
+    int RegisterSprite(const rndr::CPUImage& Image)
+    {
+        Sprite S;
+        S.Id = m_Sprites.size() + 1;
+        S.ImageData = Image;
+        m_Sprites.push_back(S);
+        return S.Id;
     }
 
     int RegisterFont(const std::string& FontPath, int FontSizeInPixels)
@@ -323,7 +372,7 @@ public:
         F.ScaledDescent = roundf(F.Scale * Descent);
         F.ScaledLineGap = roundf(F.Scale * LineGap);
 
-        constexpr int ASCIICodePointStart = 33;
+        constexpr int ASCIICodePointStart = 32;
         constexpr int ASCIICodePointEnd = 127;
         for (int CodePoint = ASCIICodePointStart; CodePoint < ASCIICodePointEnd; CodePoint++)
         {
@@ -336,32 +385,162 @@ public:
 
             int x0, x1;
             int y0, y1;
-            stbtt_GetCodepointBitmapBox(&F.StbFontInfo, CodePoint, F.Scale, F.Scale, &x0, &y0, &x1, &y1);
-            G.ScaledYOffset = F.ScaledAscent + y0;
+            stbtt_GetCodepointBitmapBox(&F.StbFontInfo, CodePoint, F.Scale, F.Scale, &x0, &y0, &x1,
+                                        &y1);
+            G.ScaledYOffset = static_cast<float>(-y1);
             G.ScaledWidth = x1 - x0;
             G.ScaledHeight = y1 - y0;
+
+            rndr::ByteSpan GlyphData;
+            GlyphData.Size =
+                G.ScaledWidth * G.ScaledHeight * rndr::GetPixelSize(rndr::PixelFormat::R8_UNORM);
+            GlyphData.Data = new uint8_t[GlyphData.Size];
+            stbtt_MakeCodepointBitmap(&F.StbFontInfo, GlyphData.Data, G.ScaledWidth, G.ScaledHeight,
+                                      G.ScaledWidth, F.Scale, F.Scale, CodePoint);
+
+            rndr::CPUImage Im;
+            Im.Width = G.ScaledWidth;
+            Im.Height = G.ScaledHeight;
+            Im.Format = rndr::PixelFormat::R8_UNORM;
+            Im.Data = GlyphData;
+            G.SpriteId = RegisterSprite(Im);
+            G.CodePoint = CodePoint;
 
             F.Glyphs.push_back(G);
         }
 
+        F.Id = m_Fonts.size() + 1;
         m_Fonts.push_back(F);
 
         return true;
     }
 
-    void PackSprites() {}
+    void WriteToAtlas(Sprite& S, AtlasPacker::RectOut& Rect, rndr::ByteSpan& Atlas)
+    {
+        S.AtlasIndex = 1;
+        S.TexBottomLeft.X = Rect.BottomLeft.X / AtlasWidth;
+        S.TexBottomLeft.Y = Rect.BottomLeft.Y / AtlasHeight;
+        S.TexTopRight.X = (Rect.BottomLeft.X + Rect.Size.X) / AtlasWidth;
+        S.TexTopRight.Y = (Rect.BottomLeft.Y + Rect.Size.Y) / AtlasHeight;
 
-    bool AddElement(const math::Point2& BottomLeft, const math::Vector2& Size, const math::Vector4& Color)
+        const int DstPixelSize = rndr::GetPixelSize(rndr::PixelFormat::R8G8B8A8_UNORM_SRGB);
+        assert(DstPixelSize == sizeof(uint32_t));
+        const int SrcPixelSize = rndr::GetPixelSize(S.ImageData.Format);
+        for (int i = 0; i < Rect.Size.Y; i++)
+        {
+            for (int j = 0; j < Rect.Size.X; j++)
+            {
+                uint8_t SrcPixel = S.ImageData.Data.Data[(i * Rect.Size.X + j) * SrcPixelSize];
+                const int DstPixelAddr =
+                    ((Rect.BottomLeft.Y + i) * AtlasWidth + Rect.BottomLeft.X + j) * DstPixelSize;
+                uint32_t* DstPixel = reinterpret_cast<uint32_t*>(Atlas.Data + DstPixelAddr);
+                *DstPixel = 0xFFFFFFFF;
+                *DstPixel &= static_cast<uint32_t>(SrcPixel) << 24;
+            }
+        }
+
+        delete[] S.ImageData.Data.Data;
+        S.ImageData.Data.Data = nullptr;
+    }
+
+    void PackSprites()
+    {
+        AtlasPacker Packer(AtlasWidth, AtlasHeight, AtlasPacker::SortCriteria::Height);
+
+        std::vector<AtlasPacker::RectIn> InRects;
+        for (int i = 0; i < m_Sprites.size(); i++)
+        {
+            Sprite& S = m_Sprites[i];
+            AtlasPacker::RectIn Rect;
+            Rect.Size = IntPoint{S.ImageData.Width, S.ImageData.Height};
+            Rect.UserData = static_cast<uintptr_t>(S.Id);
+            InRects.push_back(Rect);
+        }
+
+        std::vector<AtlasPacker::RectOut> OutRects = Packer.Pack(InRects);
+        assert(OutRects.size() == InRects.size());
+
+        rndr::ByteSpan AtlasData;
+        AtlasData.Size =
+            AtlasWidth * AtlasHeight * rndr::GetPixelSize(rndr::PixelFormat::R8G8B8A8_UNORM_SRGB);
+        AtlasData.Data = new uint8_t[AtlasData.Size];
+        memset(AtlasData.Data, 0, AtlasData.Size);
+
+        for (int i = 0; i < OutRects.size(); i++)
+        {
+            int SpriteId = static_cast<int>(OutRects[i].UserData);
+            Sprite& S = *std::find_if(m_Sprites.begin(), m_Sprites.end(),
+                                      [SpriteId](const Sprite& S) { return S.Id == SpriteId; });
+            WriteToAtlas(S, OutRects[i], AtlasData);
+        }
+
+        m_TextureAtlas->Update(m_Ctx, 1, {0, 0}, {AtlasWidth, AtlasHeight}, AtlasData);
+    }
+
+    bool AddElement(const math::Point2& BottomLeft,
+                    const math::Vector2& Size,
+                    const math::Vector4& Color)
     {
         if (m_Instances.size() == m_MaxInstances)
         {
             return false;
         }
 
-        InstanceData Data{.BottomLeft = BottomLeft, .TopRight = BottomLeft + Size, .Color = Color, .AtlasIndex = 0};
+        InstanceData Data{.BottomLeft = BottomLeft,
+                          .TopRight = BottomLeft + Size,
+                          .Color = Color,
+                          .AtlasIndex = 0};
 
         m_Instances.push_back(Data);
         return true;
+    }
+
+    void AddText(const std::string& Text,
+                 int FontId,
+                 const math::Point2& BaseLineStart,
+                 const math::Vector4& Color)
+    {
+        Font* Font = nullptr;
+        for (Renderer::Font& F : m_Fonts)
+        {
+            if (F.Id == FontId)
+            {
+                Font = &F;
+                break;
+            }
+        }
+        if (!Font)
+        {
+            return;
+        }
+
+        math::Point2 Cursor = BaseLineStart;
+        for (int i = 0; i < Text.size(); i++)
+        {
+            char CodePoint = Text[i];
+
+            Glyph& G =
+                *std::find_if(Font->Glyphs.begin(), Font->Glyphs.end(),
+                              [CodePoint](const Glyph& G) { return G.CodePoint == CodePoint; });
+
+            Sprite& S = *std::find_if(m_Sprites.begin(), m_Sprites.end(),
+                                      [Id = G.SpriteId](const Sprite& S) { return Id == S.Id; });
+
+            math::Point2 BottomLeft{Cursor.X + G.ScaledLeftSideBearing,
+                                    Cursor.Y + G.ScaledYOffset};
+            InstanceData Data{.BottomLeft = BottomLeft,
+                              .TopRight = BottomLeft + math::Vector2{G.ScaledWidth, G.ScaledHeight},
+                              .TexBottomLeft = S.TexBottomLeft,
+                              .TexTopRight = S.TexTopRight,
+                              .Color = Color,
+                              .AtlasIndex = static_cast<float>(S.AtlasIndex)};
+            m_Instances.push_back(Data);
+
+            const int Kern = stbtt_GetCodepointKernAdvance(&Font->StbFontInfo, CodePoint, Text[i + 1]);
+            const float ScaledKern = std::roundf(Kern * Font->Scale);
+
+            Cursor.X += G.ScaledAdvanceWidth + ScaledKern;
+        }
     }
 
     bool Render(rndr::FrameBuffer* FrameBuffer)
@@ -398,9 +577,6 @@ private:
     std::vector<InstanceData> m_Instances;
     std::vector<Sprite> m_Sprites;
     std::vector<Font> m_Fonts;
-
-    int m_SpriteIdGenerator = 1;
-    int m_FontIdGenerator = 1;
 };
 
 class App
@@ -410,7 +586,8 @@ public:
     constexpr static int MaxAtlasCount = 2;
 
 public:
-    App(int WindowWidth, int WindowHeight) : m_WindowWidth(WindowWidth), m_WindowHeight(WindowHeight)
+    App(int WindowWidth, int WindowHeight)
+        : m_WindowWidth(WindowWidth), m_WindowHeight(WindowHeight)
     {
         m_RndrCtx = std::make_unique<rndr::RndrContext>();
         assert(m_RndrCtx.get());
@@ -423,10 +600,12 @@ public:
         m_SwapChain = m_GraphicsCtx->CreateSwapChain(Handle, m_WindowWidth, m_WindowHeight);
         assert(m_SwapChain.IsValid());
 
-        m_Renderer = std::make_unique<Renderer>(m_GraphicsCtx.Get(), MaxInstancesCount, MaxAtlasCount, GetScreenSize());
+        m_Renderer = std::make_unique<Renderer>(m_GraphicsCtx.Get(), MaxInstancesCount,
+                                                MaxAtlasCount, GetScreenSize());
         assert(m_Renderer.get());
 
-        m_Renderer->RegisterFont(BASIC2D_ASSET_DIR "/yumin.ttf", 16);
+        m_Renderer->RegisterFont(BASIC2D_ASSET_DIR "/yumin.ttf", 24);
+        m_Renderer->PackSprites();
     }
 
     ~App() = default;
@@ -442,6 +621,7 @@ public:
             m_GraphicsCtx->ClearColor(DefaultFB->ColorBuffers[0], ClearColor);
 
             m_Renderer->AddElement({100, 100}, {100, 100}, {1, 0, 0, 1});
+            m_Renderer->AddText("The Five Boxing Wizards Jump Quickly!", 1, {100, 300}, {1, 1, 0, 1});
             m_Renderer->Render(m_SwapChain->FrameBuffer);
 
             m_GraphicsCtx->Present(m_SwapChain.Get(), true);
@@ -466,6 +646,8 @@ private:
 
 int main()
 {
+    AtlasPacker::Test();
+
     App MainApp(800, 600);
     MainApp.RunLoop();
     return 0;
