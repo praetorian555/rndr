e9532480fae60ccc5a01e83b803bf5a2a72be6d9
Marko Kostic <archmarko92@gmail.com>
2022-12-28 23:43:32 +0000

    Add clang-tidy command

---
+++ cmake/clang-tidy.cmake
@@ -0,0 +1,32 @@
+find_program(CLANG_TIDY "clang-tidy")
+
+if (CLANG_TIDY)
+
+	file(GLOB_RECURSE FILES_TO_FORMAT
+		${PROJECT_SOURCE_DIR}/include/*.h
+		${PROJECT_SOURCE_DIR}/include/*.hpp
+		${PROJECT_SOURCE_DIR}/src/*.c
+		${PROJECT_SOURCE_DIR}/src/*.cpp
+		${PROJECT_SOURCE_DIR}/src/*.h
+		${PROJECT_SOURCE_DIR}/src/*.hpp)
+
+	add_custom_target(
+        clang-tidy
+        COMMAND clang-tidy
+        -p ${PROJECT_SOURCE_DIR}/build 
+        -checks=-*,clang-analyzer-*,concurrency-*,cppcoreguidelines-*,modernize-*,performance-*,readability-*
+        ${FILES_TO_FORMAT}
+        --
+        -I${PROJECT_SOURCE_DIR}/include
+		-I${PROJECT_SOURCE_DIR}/src
+        -I${PROJECT_SOURCE_DIR}/extern/math/include
+        -DRNDR_DX11
+        -DWIN32
+    )
+	set_target_properties(clang-tidy PROPERTIES FOLDER CustomCommands)
+
+else()
+
+    message(WARNING "clang-tidy not found, clang-tidy target will not work!")
+
+endif()
