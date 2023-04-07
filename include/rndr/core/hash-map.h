01c216865137acf41aa4e4df2b16988d4c22705d
Marko Kostic <archmarko92@gmail.com>
2023-04-07 16:55:36 +0100

    Add hash-map class

---
+++ include/rndr/core/hash-map.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <unordered_map>
+
+#include "rndr/core/std-allocator.h"
+
+namespace rndr
+{
+/**
+ * A hash map allocated on the heap.
+ * @tparam Key The key type.
+ * @tparam Value The value type.
+ * @tparam Hash The hash function.
+ */
+template <typename Key, typename Value, typename Hash>
+using HashMap = std::
+    unordered_map<Key, Value, Hash, std::equal_to<Key>, StdAllocator<std::pair<const Key, Value>>>;
+}
