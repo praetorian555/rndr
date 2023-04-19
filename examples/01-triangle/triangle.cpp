f7585bac115b971a07869dc4721d0ad9381e708c
Marko Kostic <archmarko92@gmail.com>
2023-04-19 19:56:04 +0100

    [01-triangle] Implement basic color clearing and swapchain presentation

--- examples/01-triangle/triangle.cpp
+++ examples/01-triangle/triangle.cpp
@@ -16,9 +16,25 @@ int main()
 
 void Run()
 {
-    const Rndr::Window window({.width = 800, .height = 600, .name = "Triangle"});
+    Rndr::Window window({.width = 800, .height = 600, .name = "Triangle"});
+    Rndr::GraphicsContext graphics_context({.window_handle = window.GetNativeWindowHandle()});
+    assert(graphics_context.IsValid());
+    Rndr::SwapChain swap_chain(graphics_context,
+                               {.width = window.GetWidth(), .height = window.GetHeight()});
+    assert(swap_chain.IsValid());
+    constexpr math::Vector4 k_clear_color{MATH_REALC(0.0),
+                                          MATH_REALC(0.0),
+                                          MATH_REALC(0.0),
+                                          MATH_REALC(1.0)};
+
+    window.on_resize.Bind([&swap_chain](int32_t width, int32_t height)
+                          { swap_chain.SetSize(width, height); });
+
     while (!window.IsClosed())
     {
         window.ProcessEvents();
+        graphics_context.Bind(swap_chain);
+        graphics_context.ClearColor(k_clear_color);
+        graphics_context.Present(swap_chain, true);
     }
 }
