aeebab39a0cf25f55220c212a24f394559059284
Marko Kostic <archmarko92@gmail.com>
2023-05-05 12:23:28 +0100

    Change stride in BufferDesc to be int32_t

--- include/rndr/core/graphics-types.h
+++ include/rndr/core/graphics-types.h
@@ -516,7 +516,7 @@ struct BufferDesc
     uint32_t size = 0;
 
     /** Size of one element, in bytes, in an array of elements. */
-    uint32_t stride = 0;
+    int32_t stride = 0;
 
     /** Offset, in bytes, from the beginning of the buffer to the first element to use. */
     uint32_t offset = 0;
