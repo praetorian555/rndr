fba8f594570e1843ef8d7a47e2e8986cdbd59527
Marko Kostic <archmarko92@gmail.com>
2023-07-02 14:12:39 +0200

    Update Setup.ps1
    Make cmake install part of the visual studio install.
    Setup path to the address sanitizer dlls.

--- Setup.ps1
+++ Setup.ps1
@@ -5,63 +5,6 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Break
 }
 
-# Specify your desired version here
-$desiredVersion = [Version]"3.26.4"
-$installed = $false
-
-# Check if cmake is installed
-try {
-    $output = cmake --version
-    $installed = $true
-}
-catch {
-    Write-Output "CMake not found. Will proceed with the installation."
-}
-
-# Check cmake version if it's installed
-if ($installed) {
-    $output = $output -split "`n"
-    $installedVersion = $output[0] -replace "cmake version ", ""
-    if ([Version]$installedVersion -lt $desiredVersion) {
-        Write-Output "CMake version is lower than desired. Will proceed with the installation of the desired version."
-        $installed = $false
-    }
-    else {
-        Write-Output "CMake of version $installedVersion or higher is already installed."
-    }
-}
-
-# Install cmake if not installed or the installed version is lower than desired
-if (-NOT $installed) {
-    $installerName = "cmake-$($desiredVersion.Major).$($desiredVersion.Minor).$($desiredVersion.Build)-windows-x86_64.msi"
-    $url = "https://cmake.org/files/v$($desiredVersion.Major).$($desiredVersion.Minor)/$installerName"
-    $outputDir = "$env:TEMP"
-
-    # Download the installer
-    Write-Output "Downloading CMake installer from $url"
-    Invoke-WebRequest -Uri $url -OutFile "$outputDir\$installerName"
-    Write-Output "Download complete."
-
-    # Compute the SHA-256 hash of the downloaded file
-    Write-Output "Validating the downloaded file..."
-    $hash = (Get-FileHash -Path "$outputDir\$installerName" -Algorithm SHA256).Hash
-    $knownGoodHash = "566fed3fa51a274609677717d0a0f38ba69ab0b3d987df53c05392a13fc211c4"
-    if ($hash -ne $knownGoodHash) {
-        Write-Error "SHA-256 hash does not match the known good hash. The file might be corrupted or tampered with."
-        Break
-    }
-    Write-Output "Validation complete."
-
-    # Install cmake
-    Write-Output "Installing CMake..."
-    Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$outputDir\$installerName`" /quiet /norestart ADD_CMAKE_TO_PATH=System" -Wait
-    Write-Output "CMake of version $desiredVersion has been installed."
-
-    # Clean up
-    Remove-Item -Path "$outputDir\$installerName"
-    Write-Output "Clean up complete."
-}
-
 # Define the URL to download the installer and the known SHA-256 checksum for that installer
 $installerUrl = "https://aka.ms/vs/17/release/vs_Community.exe"
 
@@ -72,7 +15,6 @@ $installerPath = "${Env:TEMP}\vs_Community.exe"
 Write-Output "Downloading Visual Studio 2022 Community installer from $installerUrl"
 Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
 Write-Output "Download complete."
-
 
 # Define the components to install
 # Replace these component IDs with the ones you want to install
@@ -87,12 +29,85 @@ $components = @(
     "--add Microsoft.VisualStudio.Component.VC.ATL"
     "--add Microsoft.VisualStudio.Component.VC.Redist.14.Latest"
     "--add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core"
+    "--add Microsoft.VisualStudio.ComponentGroup.WebToolsExtensions.CMake"
+    "--add Microsoft.VisualStudio.Component.VC.CMake.Project"
     "--add Microsoft.VisualStudio.Component.VC.ASAN"
-    "--add Microsoft.VisualStudio.Component.Windows10SDK.20348"
+    "--add Microsoft.VisualStudio.Component.Windows11SDK.22000"
     "--add Microsoft.VisualStudio.Workload.NativeDesktop")
 $components = $components -join " "
 
 # Install Visual Studio 2022 Community along with the specified components
 Write-Output "Installing Visual Studio 2022 Community..."
 Start-Process -FilePath $installerPath -ArgumentList "$components --quiet --norestart --wait" -Wait
+Remove-Item $installerPath
 Write-Output "Visual Studio 2022 Community has been installed."
+
+Write-Output "Adding path to the address sanitizer dll to the system path..."
+
+# User-specified DLL file
+$specifiedDLL = "clang_rt.asan_dynamic-x86_64.dll"
+$specifiedFolder = "Hostx64"
+
+# Get the logical disk objects using WMI
+$drives = Get-WmiObject -Class Win32_LogicalDisk
+$dllPath = $null
+
+foreach ($drive in $drives)
+{
+    # Only search on drives of type 3, i.e., local disks
+    if ($drive.DriveType -ne 3)
+    {
+        continue
+    }
+
+    $pathsToSearch = @("\Program Files\Microsoft Visual Studio\2022\Community", "\Program Files (x86)\Microsoft Visual Studio\2022\Community")
+
+    foreach ($pathToSearch in $pathsToSearch)
+    {
+        # Try/Catch block for any errors (like access denied)
+        try
+        {
+            $pathToSearch = $drive.DeviceID + $pathToSearch
+            # Find the folder
+            $folderPath = (Get-ChildItem -Path $pathToSearch -Recurse -ErrorAction Stop -Filter $specifiedFolder -Directory).FullName
+
+            # If the folder exists, search for the DLL within it
+            if ($folderPath)
+            {
+                $dllPath = Get-ChildItem -Path $folderPath -Recurse -ErrorAction Stop -Filter $specifiedDLL
+                if ($dllPath)
+                {
+                    break
+                }
+            }
+        }
+        catch
+        {
+            Write-Host "Error searching drive $( $drive.DeviceID ): $( $_.Exception.Message )"
+        }
+    }
+
+    # Check if DLL was found
+    if ($dllPath -ne $null) {
+        break
+    }
+}
+
+# Check if DLL was found
+if ($dllPath -eq $null) {
+    Write-Host "Specified DLL not found"
+    return
+}
+$dllPath = (Get-Item $($dllPath.FullName)).DirectoryName
+# Get the current system path
+$systemPath = [Environment]::GetEnvironmentVariable("Path", "Machine")
+
+# Check if the DLL path is already in the system path
+if ($systemPath -notlike "*$dllPath*") {
+    # Add the DLL path to the system path
+    $newPath = $systemPath + ";" + $dllPath
+    [Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
+    Write-Host "Add path $dllPath to system path"
+} else {
+    Write-Host "Path $dllPath is already in the system path"
+}
