82b35a3f02df1466f3a5e6c3f03e3e2c0323d840
Marko Kostic <archmarko92@gmail.com>
2023-10-22 12:16:24 +0200

    Add unit tests for ProjectionCamera class

--- tests/camera-test.cpp
+++ tests/camera-test.cpp
@@ -1,87 +1,71 @@
 #include <catch2/catch_test_macros.hpp>
 
-// Generate unit tests for ProjectionCamera class using catch2.
-
-#include "math/math.h"
-
 #include "rndr/core/projection-camera.h"
 
 TEST_CASE("Perspective", "ProjectionCamera")
 {
-    constexpr float kVerticalFOV = 90.0f;
-    constexpr float kNearPlane = 0.1f;
-    constexpr float kFarPlane = 1000.0f;
-    constexpr int kScreenWidth = 1920;
-    constexpr int kScreenHeight = 1080;
-    constexpr float kAspectRatio =
-        static_cast<float>(kScreenWidth) / static_cast<float>(kScreenHeight);
+    constexpr float k_vertical_fov = 90.0f;
+    constexpr float k_near_plane = 0.1f;
+    constexpr float k_far_plane = 1000.0f;
+    constexpr int k_screen_width = 1920;
+    constexpr int k_screen_height = 1080;
+    constexpr float k_aspect_ratio = static_cast<float>(k_screen_width) / static_cast<float>(k_screen_height);
 
-    // Replace the VerticalFOV literal with a constant.
-    rndr::ProjectionCameraProperties Props;
-    Props.VerticalFOV = kVerticalFOV;
-    Props.Near = kNearPlane;
-    Props.Far = kFarPlane;
-    rndr::ProjectionCamera Camera(math::Point3{0.0f, 0.0f, 0.0f}, math::Rotator{0.0f, 0.0f, 0.0f},
-                                  kScreenWidth, kScreenHeight, Props);
+    Rndr::ProjectionCameraDesc desc;
+    desc.vertical_fov = k_vertical_fov;
+    desc.near = k_near_plane;
+    desc.far = k_far_plane;
+    Rndr::ProjectionCamera camera(Rndr::Point3f{0.0f, 0.0f, 0.0f}, Rndr::Rotatorf{0.0f, 0.0f, 0.0f}, k_screen_width, k_screen_height, desc);
 
-    REQUIRE(Camera.GetPosition() == math::Point3{0.0f, 0.0f, 0.0f});
-    REQUIRE(Camera.GetRotation() == math::Rotator{0.0f, 0.0f, 0.0f});
+    REQUIRE(camera.GetPosition() == Rndr::Point3f{0.0f, 0.0f, 0.0f});
+    REQUIRE(camera.GetRotation() == Rndr::Rotatorf{0.0f, 0.0f, 0.0f});
 
-    // Add test checks for GetProperties.
-    REQUIRE(Camera.GetProperties().VerticalFOV == kVerticalFOV);
-    REQUIRE(Camera.GetProperties().Near == kNearPlane);
-    REQUIRE(Camera.GetProperties().Far == kFarPlane);
+    REQUIRE(camera.GetDesc().vertical_fov == k_vertical_fov);
+    REQUIRE(camera.GetDesc().near == k_near_plane);
+    REQUIRE(camera.GetDesc().far == k_far_plane);
 
-    REQUIRE(Camera.FromCameraToNDC().GetMatrix() ==
-            math::Perspective_LH_N0(kVerticalFOV, kAspectRatio, kNearPlane, kFarPlane));
+    REQUIRE(camera.FromCameraToNDC() == Math::Perspective_RH_N1(k_vertical_fov, k_aspect_ratio, k_near_plane, k_far_plane));
 
-    Camera.SetPositionAndRotation(math::Point3{1.0f, 2.0f, 3.0f}, math::Rotator{1.0f, 2.0f, 3.0f});
-    REQUIRE(Camera.GetPosition() == math::Point3{1.0f, 2.0f, 3.0f});
-    REQUIRE(Camera.GetRotation() == math::Rotator{1.0f, 2.0f, 3.0f});
+    camera.SetPositionAndRotation(Rndr::Point3f{1.0f, 2.0f, 3.0f}, Rndr::Rotatorf{1.0f, 2.0f, 3.0f});
+    REQUIRE(camera.GetPosition() == Rndr::Point3f{1.0f, 2.0f, 3.0f});
+    REQUIRE(camera.GetRotation() == Rndr::Rotatorf{1.0f, 2.0f, 3.0f});
 }
 
-// Write a test same as the above one but this time make an orthographic camera.
 TEST_CASE("Orthographic", "ProjectionCamera")
 {
-    constexpr float kNearPlane = 0.0f;
-    constexpr float kFarPlane = 1000.0f;
-    constexpr int kScreenWidth = 1920;
-    constexpr int kScreenHeight = 1080;
-    constexpr int kOrthographicWidth = 100;
-    constexpr float kAspectRatio =
-        static_cast<float>(kScreenWidth) / static_cast<float>(kScreenHeight);
+    constexpr float k_near_plane = 0.0f;
+    constexpr float k_far_plane = 1000.0f;
+    constexpr int k_screen_width = 1920;
+    constexpr int k_screen_height = 1080;
+    constexpr int k_orthographic_width = 100;
+    constexpr float k_aspect_ratio = static_cast<float>(k_screen_width) / static_cast<float>(k_screen_height);
 
-    // Replace the VerticalFOV literal with a constant.
-    rndr::ProjectionCameraProperties Props;
-    Props.Near = kNearPlane;
-    Props.Far = kFarPlane;
-    Props.Projection = rndr::ProjectionType::Orthographic;
-    Props.OrthographicWidth = kOrthographicWidth;
-    rndr::ProjectionCamera Camera(math::Point3{0.0f, 0.0f, 0.0f}, math::Rotator{0.0f, 0.0f, 0.0f},
-                                  kScreenWidth, kScreenHeight, Props);
+    Rndr::ProjectionCameraDesc desc;
+    desc.near = k_near_plane;
+    desc.far = k_far_plane;
+    desc.projection = Rndr::ProjectionType::Orthographic;
+    desc.orthographic_width = k_orthographic_width;
+    Rndr::ProjectionCamera camera(Rndr::Point3f{0.0f, 0.0f, 0.0f}, Rndr::Rotatorf{0.0f, 0.0f, 0.0f}, k_screen_width, k_screen_height, desc);
 
-    REQUIRE(Camera.GetPosition() == math::Point3{0.0f, 0.0f, 0.0f});
-    REQUIRE(Camera.GetRotation() == math::Rotator{0.0f, 0.0f, 0.0f});
+    REQUIRE(camera.GetPosition() == Rndr::Point3f{0.0f, 0.0f, 0.0f});
+    REQUIRE(camera.GetRotation() == Rndr::Rotatorf{0.0f, 0.0f, 0.0f});
 
     // Add test checks for GetProperties.
-    REQUIRE(Camera.GetProperties().Near == kNearPlane);
-    REQUIRE(Camera.GetProperties().Far == kFarPlane);
-    REQUIRE(Camera.GetProperties().Projection == rndr::ProjectionType::Orthographic);
-    REQUIRE(Camera.GetProperties().OrthographicWidth == kOrthographicWidth);
+    REQUIRE(camera.GetDesc().near == k_near_plane);
+    REQUIRE(camera.GetDesc().far == k_far_plane);
+    REQUIRE(camera.GetDesc().projection == Rndr::ProjectionType::Orthographic);
+    REQUIRE(camera.GetDesc().orthographic_width == k_orthographic_width);
 
-    const float HalfWidth = kOrthographicWidth / 2.0f;
-    const float HalfHeight = HalfWidth / kAspectRatio;
-    REQUIRE(Camera.FromCameraToNDC().GetMatrix() == math::Orthographic_LH_N0(-HalfWidth, HalfWidth,
-                                                                            -HalfHeight, HalfHeight,
-                                                                            kNearPlane, kFarPlane));
+    const float half_width = k_orthographic_width / 2.0f;
+    const float half_height = half_width / k_aspect_ratio;
+    REQUIRE(camera.FromCameraToNDC() ==
+            Math::Orthographic_RH_N1(-half_width, half_width, -half_height, half_height, k_near_plane, k_far_plane));
 
-    // Write test for SetPositionAndRotation but both position and rotation are not zero.
-    Camera.SetPositionAndRotation(math::Point3{1.0f, 2.0f, 3.0f}, math::Rotator{1.0f, 2.0f, 3.0f});
-    REQUIRE(Camera.GetPosition() == math::Point3{1.0f, 2.0f, 3.0f});
-    REQUIRE(Camera.GetRotation() == math::Rotator{1.0f, 2.0f, 3.0f});
+    camera.SetPositionAndRotation(Rndr::Point3f{1.0f, 2.0f, 3.0f}, Rndr::Rotatorf{1.0f, 2.0f, 3.0f});
+    REQUIRE(camera.GetPosition() == Rndr::Point3f{1.0f, 2.0f, 3.0f});
+    REQUIRE(camera.GetRotation() == Rndr::Rotatorf{1.0f, 2.0f, 3.0f});
 
-    math::Transform Ref = math::Translate(math::Vector3{1.0f, 2.0f, 3.0f}) *
-                          math::Rotate(math::Rotator{1.0f, 2.0f, 3.0f});
-    REQUIRE(Camera.FromCameraToWorld() == Ref);
-    REQUIRE(Camera.FromWorldToCamera().GetMatrix() == Ref.GetInverse());
+    const Rndr::Matrix4x4f ref = Math::Translate(Rndr::Vector3f{1.0f, 2.0f, 3.0f}) * Math::Rotate(Rndr::Rotatorf{1.0f, 2.0f, 3.0f});
+    REQUIRE(camera.FromCameraToWorld() == ref);
+    REQUIRE(camera.FromWorldToCamera() == Math::Inverse(ref));
 }
