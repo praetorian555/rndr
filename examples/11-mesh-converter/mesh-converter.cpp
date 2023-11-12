ecd8081829aa98225082df26ede26ab62c84119d
Marko Kostic <archmarko92@gmail.com>
2023-11-12 15:42:39 +0100

    [11-mesh-converter] Add UI for mesh converter tool

--- examples/11-mesh-converter/mesh-converter.cpp
+++ examples/11-mesh-converter/mesh-converter.cpp
@@ -1,4 +1,19 @@
-#include "rndr/rndr.h"
+#define NOMINMAX
+#include <windows.h>
+#include <commdlg.h>
+#undef near
+#undef far
+
+#include <imgui.h>
+
+#include <filesystem>
+
+#include "rndr/core/containers/scope-ptr.h"
+#include "rndr/core/containers/string.h"
+#include "rndr/core/renderer-base.h"
+#include "rndr/core/window.h"
+#include "rndr/render-api/render-api.h"
+#include "rndr/utility/imgui-wrapper.h"
 
 class UIRenderer : public Rndr::RendererBase
 {
@@ -7,9 +22,16 @@ public:
     ~UIRenderer();
 
     bool Render() override;
+
+    [[nodiscard]] Rndr::String GetOutputFilePath() const { return m_output_file_path; }
+
+private:
+    Rndr::String m_selected_file_path;
+    Rndr::String m_output_file_path;
 };
 
 void Run();
+Rndr::String OpenFileDialog();
 
 int main()
 {
@@ -63,6 +85,37 @@ UIRenderer::~UIRenderer()
 bool UIRenderer::Render()
 {
     Rndr::ImGuiWrapper::StartFrame();
+
+    ImGui::Begin("Mesh Converter Tool");
+    if (ImGui::Button("Select file to convert..."))
+    {
+        m_selected_file_path = OpenFileDialog();
+        const std::filesystem::path path(m_selected_file_path);
+        m_output_file_path = path.parent_path().string() + "\\" + path.stem().string() + ".rndr";
+    }
+    ImGui::Text("Selected file: %s", !m_selected_file_path.empty() ? m_selected_file_path.c_str() : "None");
+    ImGui::Text("Output file: %s", !m_output_file_path.empty() ? m_output_file_path.c_str() : "None");
+    ImGui::Button("Convert");
+    ImGui::End();
+
     Rndr::ImGuiWrapper::EndFrame();
     return true;
+}
+
+Rndr::String OpenFileDialog()
+{
+    OPENFILENAME ofn;
+    char fileName[MAX_PATH] = "";
+    ZeroMemory(&ofn, sizeof(ofn));
+
+    ofn.lStructSize = sizeof(OPENFILENAME);
+    ofn.hwndOwner = NULL;  // If you have a window to center over, put its HANDLE here
+    ofn.lpstrFilter = "Any File\0*.*\0";
+    ofn.lpstrFile = fileName;
+    ofn.nMaxFile = MAX_PATH;
+    ofn.lpstrTitle = "Select a File";
+    ofn.Flags = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST;
+
+    GetOpenFileName(&ofn);
+    return fileName;
 }
