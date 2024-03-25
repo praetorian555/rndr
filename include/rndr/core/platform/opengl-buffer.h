8fdf6bf111f32e54e5dfae669cbebc81cc201eb5
Marko Kostic <archmarko92@gmail.com>
2024-03-25 12:50:38 +0100

    [core][opengl] Add helper constructor for Buffer

--- include/rndr/core/platform/opengl-buffer.h
+++ include/rndr/core/platform/opengl-buffer.h
@@ -28,6 +28,19 @@ public:
      */
     Buffer(const GraphicsContext& graphics_context, const BufferDesc& desc, const ConstByteSpan& init_data = ConstByteSpan{});
 
+    /**
+     * Create a new buffer with initial data.
+     * @tparam DataType The type of the data to initialize the buffer with.
+     * @param graphics_context The graphics context to create the buffer with.
+     * @param init_data The initial data to fill the buffer with.
+     * @param type The type of the buffer. Default is Vertex.
+     * @param usage The usage of the buffer. Default is Default.
+     * @param offset The offset in the buffer to start writing the data. Default is 0.
+     */
+    template <typename DataType>
+    Buffer(const GraphicsContext& graphics_context, const Array<DataType>& init_data, BufferType type, Usage usage = Rndr::Usage::Default,
+           uint32_t offset = 0);
+
     ~Buffer();
     Buffer(const Buffer&) = delete;
     Buffer& operator=(const Buffer&) = delete;
@@ -45,5 +58,18 @@ private:
     BufferDesc m_desc;
     GLuint m_native_buffer = k_invalid_opengl_object;
 };
+
+template <typename DataType>
+Rndr::Buffer::Buffer(const Rndr::GraphicsContext& graphics_context, const Rndr::Array<DataType>& init_data, Rndr::BufferType type,
+                     Rndr::Usage usage, uint32_t offset)
+    : Buffer(graphics_context,
+             BufferDesc{.type = type,
+                        .usage = usage,
+                        .size = ((uint32_t)init_data.size()) * sizeof(init_data[0]),
+                        .stride = sizeof(init_data[0]),
+                        .offset = offset * static_cast<uint32_t>(sizeof(init_data[0]))},
+             ConstByteSpan{reinterpret_cast<const uint8_t*>(init_data.data()), init_data.size() * sizeof(init_data[0])})
+{
+}
 
 }  // namespace Rndr
