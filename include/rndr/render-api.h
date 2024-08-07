50cdb040c05bf5855f135d06859e7114ffe4fc36
Marko Kostic <archmarko92@gmail.com>
2024-08-07 21:44:34 +0200

    Add frame buffer include in render-api.h

--- include/rndr/render-api.h
+++ include/rndr/render-api.h
@@ -5,6 +5,7 @@
 #if RNDR_OPENGL
 #include "rndr/platform/opengl-buffer.h"
 #include "rndr/platform/opengl-command-list.h"
+#include "rndr/platform/opengl-frame-buffer.h"
 #include "rndr/platform/opengl-graphics-context.h"
 #include "rndr/platform/opengl-pipeline.h"
 #include "rndr/platform/opengl-shader.h"
