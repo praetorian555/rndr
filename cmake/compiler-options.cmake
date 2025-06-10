15d1d76203e03a6e3b5340e2fb7b446d65550b18
Marko Kostic <archmarko92@gmail.com>
2025-06-10 23:08:20 +0200

    Compile win32 with UNICODE

--- cmake/compiler-options.cmake
+++ cmake/compiler-options.cmake
@@ -1,5 +1,5 @@
 function(rndr_setup_compiler_options target)
 
-    target_compile_options(${target} INTERFACE "/MP")
+    target_compile_options(${target} INTERFACE "/MP" "-DUNICODE=1")
 
 endfunction()
