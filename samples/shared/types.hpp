4ab93ed4306336f20a5efb2d7a8e2954059a96b9
Marko Kostic <archmarko92@gmail.com>
2025-10-26 13:41:28 +0100

    [Samples] Add types.h

---
+++ samples/shared/types.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "rndr/types.hpp"
+
+using i8 = Rndr::i8;
+using i16 = Rndr::i16;
+using i32 = Rndr::i32;
+using i64 = Rndr::i64;
+
+using u8 = Rndr::u8;
+using u16 = Rndr::u16;
+using u32 = Rndr::u32;
+using u64 = Rndr::u64;
+
+using f32 = Rndr::f32;
+using f64 = Rndr::f64;
