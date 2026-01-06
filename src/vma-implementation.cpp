dba84a083d350141c03bbf6b1bf56093ef00ebf8
Marko Kostic <archmarko92@gmail.com>
2026-01-06 16:20:31 +0100

    Add RNDR_ADVANCED_API library option
    It will enable more advanced API for rendering that will use Vulkan. Setup Vulkan SDK and library dependencies.

---
+++ src/vma-implementation.cpp
@@ -0,0 +1,9 @@
+#define VMA_IMPLEMENTATION
+#include "opal/defines.h"
+OPAL_START_DISABLE_WARNINGS
+OPAL_DISABLE_MSVC_WARNING(4100) // Unreferenced function argument
+OPAL_DISABLE_MSVC_WARNING(4189) // Local variable initialized but not referenced
+OPAL_DISABLE_MSVC_WARNING(4127) // Conditional expression is constant
+OPAL_DISABLE_MSVC_WARNING(4324) // Structure was padded due to alignment specifier
+#include "vma/vk_mem_alloc.h"
+OPAL_END_DISABLE_WARNINGS
