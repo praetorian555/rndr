095777d30b3bf361b2851766019c2b2e43ade929
Marko Kostic <archmarko92@gmail.com>
2026-01-09 22:47:29 +0100

    Continue implementing ModernVulkan sample

--- samples/modern-vulkan/modern-vulkan.cpp
+++ samples/modern-vulkan/modern-vulkan.cpp
@@ -1,14 +1,17 @@
+#include "../../build/opengl-msvc-opt-debug/_deps/opal-src/include/opal/paths.h"
 #include "opal/container/dynamic-array.h"
 #include "rndr/application.hpp"
+#include "rndr/file.hpp"
 
 #include "vma/vk_mem_alloc.h"
 #include "volk/volk.h"
 
-#include "rndr/types.hpp"
+#include "rndr/advanced/advanced-texture.hpp"
+#include "rndr/advanced/device.hpp"
 #include "rndr/advanced/graphics-context.hpp"
 #include "rndr/advanced/physical-device.hpp"
-#include "rndr/advanced/device.hpp"
 #include "rndr/advanced/swap-chain.hpp"
+#include "rndr/types.hpp"
 
 using i32 = Rndr::i32;
 using u32 = Rndr::u32;
@@ -45,25 +48,30 @@ int main()
     Rndr::AdvancedSurface surface(graphics_context, window->GetNativeHandle());
 
     auto physical_devices = graphics_context.EnumeratePhysicalDevices();
-    Rndr::AdvancedDevice device(std::move(physical_devices[0]), {.surface = Opal::Ref{surface}});
+    Rndr::AdvancedDevice device(std::move(physical_devices[0]), graphics_context, {.surface = Opal::Ref{surface}});
     Rndr::AdvancedDeviceQueue m_graphics_queue(device, Rndr::AdvancedDeviceQueueFamilyFlags::Graphics);
     Rndr::AdvancedDeviceQueue m_present_queue(device, Rndr::AdvancedDeviceQueueFamilyFlags::Present);
 
     Rndr::AdvancedSwapChain swap_chain(device, surface, {});
 
-    // Setup GPU allocator
-    const VmaVulkanFunctions vk_functions {
-        .vkGetInstanceProcAddr = vkGetInstanceProcAddr,
-        .vkGetDeviceProcAddr = vkGetDeviceProcAddr,
-        .vkCreateImage = vkCreateImage,
-    };
-    const VmaAllocatorCreateInfo vma_alloc_create_info = {
-        .flags = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT,
-        .physicalDevice = device.GetNativePhysicalDevice(),
-        .device = device.GetNativeDevice(),
-        .pVulkanFunctions = &vk_functions,
-        .instance = graphics_context.GetInstance(),
-    };
-    VK_CHECK(vmaCreateAllocator(&vma_alloc_create_info, &vma_allocator));
+    const VkExtent2D extent = swap_chain.GetExtent();
+    Rndr::AdvancedTexture texture(device, {.image_type = VK_IMAGE_TYPE_2D,
+                                           .format = VK_FORMAT_D32_SFLOAT,
+                                           .width = extent.width,
+                                           .height = extent.height,
+                                           .depth = 1,
+                                           .mip_levels = 1,
+                                           .array_layers = 1,
+                                           .sample_count = VK_SAMPLE_COUNT_1_BIT,
+                                           .image_usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+                                           .view_type = VK_IMAGE_VIEW_TYPE_2D,
+                                           .aspect_mask = VK_IMAGE_ASPECT_DEPTH_BIT,
+                                           .mip_map_level_count = 1,
+                                           .layer_count = 1});
+    // TODO: Flip y for everything in mesh for Vulkan
+    Rndr::Mesh mesh;
+    Rndr::MaterialDesc material_desc;
+    const Opal::StringUtf8 mesh_path = Opal::Paths::Combine(RNDR_CORE_ASSETS_DIR, "sample-models", "Suzanne", "glTF", "Suzanne.gltf");
+    Rndr::File::LoadMeshAndMaterialDescription(mesh_path, mesh, material_desc);
 
 }
